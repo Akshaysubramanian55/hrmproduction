16f42a4e732d59b7c8c1894b8346c33ee3755757
akshaysubramanian <akshaysubramanian55@gmail.com>
2024-04-03 14:08:14 +0530

    april3 commit

--- hrmserver/db/models/revokedtokens.js
+++
@@ -1,9 +0,0 @@
-// const mongoose=require('mongoose');
-
-// const accessControl= new mongoose.Schema(
-//     {
-//         token:"string"
-//     }
-// );
-
-// module.exports=mongoose.model("revoked tokens",accessControl)
